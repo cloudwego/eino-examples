36d0df4c4bab9084d1a31220509bf6e8eae84685
shentong.martin <shentong.martin@bytedance.com>
2025-07-10 19:02:35 +0800

    feat: add examples for adk
    Change-Id: I207d1a2174d16c20d47d561d9e51f900e2d078fb

---
+++ adk/layered_supervisor/layered_supervisor_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/cloudwego/eino/adk"
+	"github.com/cloudwego/eino/schema"
+
+	adk2 "github.com/cloudwego/eino-examples/adk"
+)
+
+func TestLayeredSupervisor(t *testing.T) {
+	sv, err := buildSupervisor(context.Background())
+	if err != nil {
+		log.Fatalf("build layered superviser failed: %v", err)
+	}
+
+	query := "find US and New York state GDP in 2024. what % of US GDP was New York state? " +
+		"Then multiply that percentage by 1.589."
+
+	iter := sv.Run(context.Background(), &adk.AgentInput{
+		Messages: []adk.Message{
+			schema.UserMessage(query),
+		},
+		EnableStreaming: true,
+	})
+
+	fmt.Println("\nuser query: ", query)
+
+	for {
+		event, hasEvent := iter.Next()
+		if !hasEvent {
+			break
+		}
+
+		adk2.PrintEvent(event)
+	}
+}
