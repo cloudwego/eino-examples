7f7a48fc5b5103db3d56f6fb4a88d53d82f22663
lipandeng <lipandeng@bytedance.com>
2025-09-01 20:06:12 +0800

    feat: fix ark ChatModel

--- adk/common/model/chat_model.go
+++ adk/common/model/chat_model.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cloudwego/eino-ext/components/model/ark"
 	"github.com/cloudwego/eino-ext/components/model/openai"
 	"github.com/cloudwego/eino/components/model"
+	arkModel "github.com/volcengine/volcengine-go-sdk/service/arkruntime/model"
 )
 
 func NewChatModel() model.ToolCallingChatModel {
@@ -34,9 +35,12 @@ func NewChatModel() model.ToolCallingChatModel {
 	if modelType == "ark" {
 		cm, err := ark.NewChatModel(context.Background(), &ark.ChatModelConfig{
 			// Add Ark-specific configuration from environment variables
-			APIKey:   os.Getenv("ARK_API_KEY"),
-			Model:    os.Getenv("ARK_MODEL"),
-			BaseURL:  os.Getenv("ARK_BASE_URL"),
+			APIKey:  os.Getenv("ARK_API_KEY"),
+			Model:   os.Getenv("ARK_MODEL"),
+			BaseURL: os.Getenv("ARK_BASE_URL"),
+			Thinking: &arkModel.Thinking{
+				Type: arkModel.ThinkingTypeDisabled,
+			},
 		})
 		if err != nil {
 			log.Fatalf("ark.NewChatModel failed: %v", err)
