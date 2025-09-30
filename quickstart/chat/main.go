ef645b5c90883a4077ba7a735c050cf96155ca19
MichaelChow <chonger@MichaelChowdeMacBook-Pro.local>
2025-09-30 13:45:46 +0800

    refactor(chat): rename llm variable to cm (chatModel) for better naming consistency

--- quickstart/chat/main.go
+++ quickstart/chat/main.go
@@ -29,17 +29,17 @@ func main() {
 	messages := createMessagesFromTemplate()
 	log.Printf("messages: %+v\n\n", messages)
 
-	// 创建llm
-	log.Printf("===create llm===\n")
-	cm := createOpenAIChatModel(ctx)
-	// cm := createOllamaChatModel(ctx)
-	log.Printf("create llm success\n\n")
+	// 创建chatModel
+	log.Printf("===create chatModel===\n")
+	// cm := createOpenAIChatModel(ctx)
+	cm := createOllamaChatModel(ctx)
+	log.Printf("create chatModel success\n\n")
 
-	log.Printf("===llm generate===\n")
+	log.Printf("===chatModel generate===\n")
 	result := generate(ctx, cm, messages)
 	log.Printf("result: %+v\n\n", result)
 
-	log.Printf("===llm stream generate===\n")
+	log.Printf("===chatModel stream generate===\n")
 	streamResult := stream(ctx, cm, messages)
 	reportStream(streamResult)
 }
