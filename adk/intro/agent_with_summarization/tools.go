2e098104b446c799ce3818dca2df49ee1f53a59f
lipandeng <lipandeng@bytedance.com>
2025-11-20 16:31:03 +0800

    fix(adk): fix ut

--- adk/intro/agent_with_summarization/tools.go
+++ adk/intro/agent_with_summarization/tools.go
@@ -29,49 +29,49 @@ import (
 )
 
 type RepeatSectionsInput struct {
-    Title        string   `json:"title" jsonschema_description:"Section title"`
-    Paragraphs   []string `json:"paragraphs" jsonschema_description:"Paragraphs to be repeated"`
-    RepeatCount  int      `json:"repeat_count" jsonschema_description:"Times to repeat paragraphs, default 2"`
+	Title       string   `json:"title" jsonschema_description:"Section title"`
+	Paragraphs  []string `json:"paragraphs" jsonschema_description:"Paragraphs to be repeated"`
+	RepeatCount int      `json:"repeat_count" jsonschema_description:"Times to repeat paragraphs, default 2"`
 }
 
 func NewRepeatSectionsTool() tool.InvokableTool {
-    t, err := utils.InferTool(
-        "repeat_sections",
-        "Repeat given paragraphs to quickly accumulate context",
-        func(ctx context.Context, in *RepeatSectionsInput) (string, error) {
-            if in.RepeatCount <= 0 {
-                in.RepeatCount = 2
-            }
-            var b strings.Builder
-            b.WriteString("## ")
-            b.WriteString(in.Title)
-            b.WriteString("\n")
-            idx := 0
-            for i := 0; i < in.RepeatCount; i++ {
-                for _, sec := range in.Paragraphs {
-                    b.WriteString(strconv.Itoa(idx + 1))
-                    b.WriteString(". ")
-                    b.WriteString(sec)
-                    b.WriteString("\n")
-                    idx++
-                }
-            }
-            callCount := 0
-            times, ok := adk.GetSessionValue(ctx, "_tool_call_count")
-            if !ok {
-                times = 0
-            } else {
-                callCount = times.(int)
-            }
-            callCount++
-            adk.AddSessionValue(ctx, "_tool_call_count", callCount)
+	t, err := utils.InferTool(
+		"repeat_sections",
+		"Repeat given paragraphs to quickly accumulate context",
+		func(ctx context.Context, in *RepeatSectionsInput) (string, error) {
+			if in.RepeatCount <= 0 {
+				in.RepeatCount = 2
+			}
+			var b strings.Builder
+			b.WriteString("## ")
+			b.WriteString(in.Title)
+			b.WriteString("\n")
+			idx := 0
+			for i := 0; i < in.RepeatCount; i++ {
+				for _, sec := range in.Paragraphs {
+					b.WriteString(strconv.Itoa(idx + 1))
+					b.WriteString(". ")
+					b.WriteString(sec)
+					b.WriteString("\n")
+					idx++
+				}
+			}
+			callCount := 0
+			times, ok := adk.GetSessionValue(ctx, "_tool_call_count")
+			if !ok {
+				times = 0
+			} else {
+				callCount = times.(int)
+			}
+			callCount++
+			adk.AddSessionValue(ctx, "_tool_call_count", callCount)
 
-            b.WriteString(fmt.Sprintf("Tool calls so far: %d", callCount))
-            return b.String(), nil
-        },
-    )
-    if err != nil {
-        log.Fatalf("failed to create repeat_sections tool: %v", err)
-    }
-    return t
+			b.WriteString(fmt.Sprintf("Tool calls so far: %d", callCount))
+			return b.String(), nil
+		},
+	)
+	if err != nil {
+		log.Fatalf("failed to create repeat_sections tool: %v", err)
+	}
+	return t
 }
